6b522c8af39a37e46e7ed690fdf0f0eade2fb77c
josefalk <125025821+josefalk@users.noreply.github.com>
2024-09-13 01:27:43 +0200

    Update neopixel_control.py

--- neopixel_control.py
+++ neopixel_control.py
@@ -1,38 +1,14 @@
-# neopixel_control.py
-
-import machine
 import neopixel
-import time
+import machine
 
-# Define the pin where the NeoPixel is connected and the number of NeoPixels
-NEO_PIN = 16  # Change this if the NeoPixel is on another pin
-NUM_PIXELS = 1  # Adjust this if there are more NeoPixels
+# Define the pin and number of NeoPixels
+pixel_pin = 16
+num_pixels = 1
 
-# Initialize NeoPixel object
-np = neopixel.NeoPixel(machine.Pin(NEO_PIN), NUM_PIXELS)
+# Initialize the NeoPixel
+pixel = neopixel.NeoPixel(machine.Pin(pixel_pin), num_pixels)
 
-def wheel(pos):
-    # Input a value 0 to 255 to get a color value.
-    # The colors are a transition of red - green - blue.
-    if pos < 85:
-        return (255 - pos * 3, pos * 3, 0)
-    elif pos < 170:
-        pos -= 85
-        return (0, 255 - pos * 3, pos * 3)
-    else:
-        pos -= 170
-        return (pos * 3, 0, 255 - pos * 3)
-
-def color_change(speed=10):
-    # Loops through colors and gradually changes the NeoPixel color
-    while True:
-        for i in range(255):
-            np[0] = wheel(i)
-            np.write()
-            time.sleep_ms(speed)
-
-def turn_off():
-    # Function to turn off the NeoPixel
-    np[0] = (0, 0, 0)
-    np.write()
-
+def color(r, g, b):
+    """Set the NeoPixel to the specified RGB color."""
+    pixel[0] = (r, g, b)
+    pixel.write()
