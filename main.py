7de85b9f1bce74587917281f0ab7e60d3316f944
josefalk <125025821+josefalk@users.noreply.github.com>
2024-09-13 01:29:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 #main.py
 import motor_control
-import neopixel_control
-
-# Run the motor test sequence
-motor_control.test_motor()
 
 
-try:
-    # Start the color change function with a specified speed
-    neopixel_control.color_change(speed=20)
-except KeyboardInterrupt:
-    # Turn off NeoPixel when the program is interrupted
-    neopixel_control.turn_off()
